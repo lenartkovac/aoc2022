d1203cedb61e0ed5fa327b4ec65b35748c0ceb2b
Lenart Kovac <kovac.lenart@gmail.com>
2022-12-03 00:36:23 +0100

    day2

--- day2/description.md
+++ day2/description.md
@@ -2,6 +2,8 @@ source: https://adventofcode.com/2022/day/2
 
 # Day 2: Rock Papers Scissors 
 
+## Part One
+
 The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant _Rock Paper Scissors tournament_ is already in progress.
 
 Rock Paper Scissors isa game between two players. Each game contains many rounds; in each round, the players each simultaneously choose one of Rock, Paper, or Scissors using a hand shape.
@@ -36,3 +38,18 @@ This strategy guide predicts and recommends the following:
 In this example, if you were to follow the strategy guide, you would get a total score of `15` (8 + 1 + 6).
 
 **What would your total score be if everything goes exactly according to your strategy guide?**
+
+## Part Two
+
+The Elf finishes helping with the tent and sneaks back over to you.
+"Anyway, the second column says how the rounds needs to end: `X` means you need to lose, `Y` means you need to end the round in a draw, and `Z` means you need to won. Good luck!!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+- In the first round, your opponent will chose Rock (`A`), and you need the round to end in a draw (`Y`), so you also choose Rock. This gives you a score 1 + 3 = **4**
+- In the second round, your opponent will choose Paper (`B`), and you choose Rock so you lose (`X`) with a score of 1 + 0 = **1**.
+- In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = **7**.
+
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of `12`.
+
+Following the Elf's instructions for the second column, **what would your total score be if everything goes exactly according to your strategy guide?**
---
+++ day2/solution.hs
@@ -0,0 +1,83 @@
+import System.IO
+
+data Symbol = Rock | Paper | Scissors deriving(Show)
+data Result = Win | Draw | Loss deriving(Show)
+
+main = do
+    putStrLn "insert file"
+    file <- getLine
+    putStrLn ("reading from file " ++ file)
+    content <- readFile file
+
+    let linesOfFiles = lines content
+    let matches1 = map (\[x, y] -> ((mapSymbol x, mapSymbol y)))(map words linesOfFiles)
+    let matches2 = map(\[x, y] -> ((mapSymbol x, mapResult y)))(map words linesOfFiles)
+    print matches2
+    let matchResults1 = map evaluateMatch matches1
+    let matchResults2 = map evaluateMatchWithResult matches2
+    print(matchResults2)
+    let result1 = foldl (\acc el -> (((fst acc) + (fst el)), ((snd acc) + (snd el)))) (0, 0) matchResults1
+    let result2 = sum matchResults2
+    print ("End result should be ", result1)
+    print ("result 2 ", result2)
+
+
+mapSymbol :: String -> Symbol 
+mapSymbol x = 
+    case x of
+    "A" -> Rock
+    "B" -> Paper
+    "C" -> Scissors
+    "X" -> Rock
+    "Y" -> Paper
+    "Z" -> Scissors
+
+mapResult :: String -> Result
+mapResult x =
+    case x of
+    "X" -> Loss 
+    "Y" -> Draw
+    "Z" -> Win
+
+evaluateSymbol :: Symbol -> Int
+evaluateSymbol Rock = 1
+evaluateSymbol Paper = 2
+evaluateSymbol Scissors = 3
+
+evaluateResult :: Result -> Int
+evaluateResult Win = 6
+evaluateResult Draw = 3
+evaluateResult Loss = 0
+
+
+evaluateMatchup :: Symbol -> Symbol -> (Int, Int)
+evaluateMatchup Rock Rock = (3, 3)
+evaluateMatchup Paper Paper = (3, 3)
+evaluateMatchup Scissors Scissors = (3, 3)
+evaluateMatchup Paper Rock  = (6, 0)
+evaluateMatchup Rock Scissors = (6, 0)
+evaluateMatchup Scissors Paper = (6, 0)
+evaluateMatchup x y = do
+    let result = evaluateMatchup y x
+    (snd result, fst result)
+
+evaluateMatch :: (Symbol, Symbol) -> (Int, Int)
+evaluateMatch (x, y) = do
+    let matchup = evaluateMatchup x y
+    ((evaluateSymbol x) + (fst matchup), (evaluateSymbol y) + (snd matchup))
+
+evaluateMatchWithResult :: (Symbol, Result) -> (Int)
+evaluateMatchWithResult (x, y) = do
+    let resultValue = evaluateResult y
+    let resultSymbol = getResponseSymbolForResult x y
+    (evaluateSymbol resultSymbol) + resultValue
+
+getResponseSymbolForResult :: Symbol -> Result -> Symbol
+getResponseSymbolForResult x Draw = x
+getResponseSymbolForResult Rock Win = Paper 
+getResponseSymbolForResult Rock Loss = Scissors
+getResponseSymbolForResult Paper Win = Scissors 
+getResponseSymbolForResult Paper Loss = Rock 
+getResponseSymbolForResult Scissors Win = Rock 
+getResponseSymbolForResult Scissors Loss = Paper 
+
